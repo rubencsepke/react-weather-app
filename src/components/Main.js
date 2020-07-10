b9184d903709e93dfb91b9e6f46c6c9fceab4b63
Ruben <ruben.csepke@gmail.com>
2020-07-10 17:39:47 +0200

    Added header and search

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react'
 
+import '../sass/style.scss'
+
 export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      query: ''
+    }
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSearch = (e) => {
+    e.key === 'Enter' && console.log(this.state.query)
+  }
+
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({query: e.target.value})
+  }
+
   render() {
     return (
-      <div>
-        
-      </div>
+      <main className="container">
+        <header className="header">
+          <h1>Weather Forecast</h1>
+          <input id="search" type="text" value={this.state.query} onChange={this.handleChange} onKeyPress={this.handleSearch}/>
+        </header>
+      </main>
     )
   }
 }
