6bd94f81fc82067d70239e088eab79bfa862a20e
Ruben <ruben.csepke@gmail.com>
2020-07-13 11:44:37 +0200

    Added CurrentWeather component

---
+++ src/components/CurrentWeather.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import '../sass/cards.scss';
+
+const CurrentWeather = (props) => {
+  return (
+    <>
+      <div className="card current-weather-card">
+        <div className="city-name">{props.data.city}</div>
+        <div className="weather-icon"><img src={`http://openweathermap.org/img/wn/${props.data.icon}@2x.png`} alt={props.data.weather}/></div>
+        <div className="weather">{props.data.weather}</div>
+        <div className="temp">{props.data.temp} &deg;C</div>
+      </div>
+      <div className="card current-info-card">
+        <div className="info-row">
+          <div className="info">
+            <span className="info-title">Humidity</span>
+            <b className="info-value">{props.data.humidity}</b>
+          </div>
+          <div className="info">
+            <span className="info-title">Pressure</span>
+            <b className="info-value">{props.data.pressure}</b>
+          </div>
+          <div className="info">
+            <span className="info-title">Wind</span>
+            <b className="info-value">{props.data.windSpeed}</b>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default CurrentWeather;
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,20 +3,39 @@ import React, { Component } from 'react'
 import '../sass/style.scss'
 
 import { getCurrentData } from '../api/fetchCurrentData.js';
+import CurrentWeather from './CurrentWeather';
 
 export default class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isLoading: false,
-      query: ''
+      query: '',
+      currentWeather: []
     }
     this.handleSearch = this.handleSearch.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleSearch = (e) => {
-    e.key === 'Enter' && getCurrentData(this.state.query);
+    if(e.key === 'Enter') {
+      this.setState({isLoading: true});
+      getCurrentData(this.state.query).then(data => {
+        const currentWeather = {
+          city: data.name,
+          lat: data.coord.lat,
+          long: data.coord.lon,
+          temp: data.main.temp,
+          humidity: data.main.humidity,
+          pressure: data.main.pressure,
+          windSpeed: data.wind.speed,
+          windDeg: data.wind.deg,
+          weather: data.weather[0].main,
+          icon: data.weather[0].icon
+        }
+        this.setState({currentWeather: currentWeather, isLoading: false});
+      })
+    }
   }
 
   handleChange = (e) => {
@@ -25,12 +44,20 @@ export default class Main extends Component {
   }
 
   render() {
+    const { isLoading, currentWeather, query } = this.state;
     return (
       <main className="container">
         <header className="header">
           <h1>Weather Forecast</h1>
-          <input id="search" type="text" value={this.state.query} onChange={this.handleChange} onKeyPress={this.handleSearch}/>
+          <input id="search" type="text" value={query} onChange={this.handleChange} onKeyPress={this.handleSearch}/>
         </header>
+        {
+          isLoading ? <p>Loading...</p> : 
+          currentWeather.length !== 0 &&
+          <div className="current-weather">
+            <CurrentWeather data={currentWeather} />
+          </div> 
+        }
       </main>
     )
   }
