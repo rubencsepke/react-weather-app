686f6e29ca8bc5870d0990368e95e6e06fa34883
Ruben <ruben.csepke@gmail.com>
2020-07-13 11:43:28 +0200

    Added units parameter and returned data

--- src/api/fetchCurrentData.js
+++ src/api/fetchCurrentData.js
@@ -1,8 +1,8 @@
 const url = 'https://api.openweathermap.org/data/2.5/weather';
 
 export const getCurrentData = async (param) => {
-  const resp = await fetch(`${url}?q=${param}&appid=${process.env.REACT_APP_API_ID}`);
+  const resp = await fetch(`${url}?q=${param}&units=metric&appid=${process.env.REACT_APP_API_ID}`);
   const data = await resp.json();
 
-  console.log(data);
+  return data;
 }
